32049811ada8d6b8504f1a29ee50d52f6f46306f
makanai5610 <takumi.k.5610@gmail.com>
2019-12-06 10:58:52 +0900

    fix layout

--- src/TextFieldComponent.tsx
+++ src/TextFieldComponent.tsx
@@ -1,16 +1,5 @@
 import React, { FC } from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-
-const userStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {},
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1)
-    }
-  })
-);
 
 interface TextFieldComponentProps {
   text: string;
@@ -18,24 +7,20 @@ interface TextFieldComponentProps {
 }
 
 const TextFieldComponent: FC<TextFieldComponentProps> = ({ text, setText }) => {
-  const classes = userStyles();
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="filled-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          className={classes.textField}
-          margin="normal"
-          variant="filled"
-          value={text}
-          onChange={setText}
-          fullWidth={true}
-        />
-      </div>
-    </form>
+    <div>
+      <TextField
+        id="filled-textarea"
+        label="Multiline Placeholder"
+        placeholder="Placeholder"
+        multiline
+        margin="normal"
+        variant="filled"
+        value={text}
+        onChange={setText}
+        fullWidth={true}
+      />
+    </div>
   );
 };
 
--- src/TextFieldContainer.tsx
+++ src/TextFieldContainer.tsx
@@ -1,11 +1,31 @@
 import React, { FC } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 import TextFieldComponent from "./TextFieldComponent";
 import useTextField from "./TextFieldHook";
 
+const useStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    }
+  })
+);
+
 const TextFieldContainer: FC = () => {
   const [text, setText] = useTextField("");
-  return <TextFieldComponent text={text} setText={setText} />;
+  const classes = useStyle();
+  return (
+    <div className={classes.root}>
+      <Grid container direction="column" justify="flex-start">
+        <Grid item>
+          <TextFieldComponent text={text} setText={setText} />
+          <p>{text}</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default TextFieldContainer;
