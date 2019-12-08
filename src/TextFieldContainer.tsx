757c7a0cb02c7c14426bd803096d7d108046b11c
makanai5610 <takumi.k.5610@gmail.com>
2019-12-08 17:44:20 +0900

    fix layout

--- src/TextFieldContainer.tsx
+++ src/TextFieldContainer.tsx
@@ -1,30 +1,15 @@
 import React, { FC } from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-
 import TextFieldComponent from "./TextFieldComponent";
 import useTextField from "./TextFieldHook";
 
-const useStyle = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      marginLeft: "10%",
-      marginRight: "10%"
-    }
-  })
-);
+import "./TextFieldContainer.css";
 
 const TextFieldContainer: FC = () => {
   const [text, setText] = useTextField("");
-  const classes = useStyle();
   return (
-    <div className={classes.root}>
-      <Paper>
-        <div className="container">
-          <TextFieldComponent text={text} setText={setText} />
-          <p>{text}</p>
-        </div>
-      </Paper>
+    <div className="container">
+      <TextFieldComponent text={text} setText={setText} />
+      <p>{text}</p>
     </div>
   );
 };
